3353133a2dde17bb5affdeb187cc811071c752af
rkteddy <838474413@qq.com>
2018-06-27 17:00:25 +0800

    Write result to xml file

--- weather.py
+++ weather.py
@@ -1,6 +1,7 @@
 from bs4 import BeautifulSoup
 from urllib.request import urlopen
 import re
+from xml.dom import minidom
 
 
 def cat_data():
@@ -20,6 +21,44 @@ def cat_data():
     return weas, his, los
 
 
+def xml_root():
+
+    weas, his, los = cat_data()
+
+    xmldoc = minidom.Document()
+
+    root = xmldoc.createElement('WeatherReport')
+    xmldoc.appendChild(root)
+
+    tem_node = xmldoc.createElement('Tempreture')
+    wea_node = xmldoc.createElement('Weather')
+    root.appendChild(tem_node)
+    root.appendChild(wea_node)
+
+    hi_node = xmldoc.createElement('Highest')
+    lo_node = xmldoc.createElement('Lowest')
+    tem_node.appendChild(hi_node)
+    tem_node.appendChild(lo_node)
+
+    for i in range(7):
+        sub_wea = xmldoc.createElement('day' + str(i))
+        sub_wea.appendChild(xmldoc.createTextNode(weas[i]))
+        wea_node.appendChild(sub_wea)
+
+        sub_hi = xmldoc.createElement('day' + str(i))
+        sub_hi.appendChild(xmldoc.createTextNode(his[i]))
+        hi_node.appendChild(sub_hi)
+
+        sub_lo = xmldoc.createElement('day' + str(i))
+        sub_lo.appendChild(xmldoc.createTextNode(los[i]))
+        lo_node.appendChild(sub_lo)
+
+    file = open('./weather.xml', 'w')
+    xmldoc.writexml(writer=file, indent='\t', addindent='\t', newl='\n', encoding='utf-8')
+    file.close()
+
+
 if __name__ == '__main__':
 
     # print(cat_data())
+    xml_root()
